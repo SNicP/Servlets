fc395dab5b2d2416e551a333e4042729b68dad61
SNicP <p375297200586@gmail.com>
2025-08-02 17:29:21 +0300

    Added dependency injection with annotations spring

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,49 +1,15 @@
 package ru.netology.repository;
 
-import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
 
-public class PostRepository {
-    private final AtomicLong idCounter = new AtomicLong(1);
-    private final ConcurrentHashMap<Long, Post> posts;
+public interface PostRepository {
+    List<Post> all();
 
-    public PostRepository() {
-        this.posts = new ConcurrentHashMap<>();
-    }
+    Optional<Post> getById(long id);
 
-    public List<Post> all() {
-        return new ArrayList<>(posts.values());
-    }
+    Post save(Post post);
 
-    public Optional<Post> getById(long id) {
-        return Optional.ofNullable(posts.get(id));
-    }
-
-    public Post save(Post post) {
-        if (post.getId() == 0) {
-            long newId = idCounter.get();
-            post.setId(newId);
-            posts.put(newId, post);
-            idCounter.getAndIncrement();
-        } else if (!posts.containsKey(post.getId()) && post.getId() != 0) {
-            throw new NotFoundException("Сообщение с id " + post.getId() + " не найдено!");
-        } else {
-            posts.get(post.getId()).setContent(post.getContent());
-        }
-        return post;
-    }
-
-    public void removeById(long id) {
-        if (posts.containsKey(id)) {
-            posts.remove(id);
-        } else {
-            throw new NotFoundException("Сообщение с id" + id + " не найдено!");
-        }
-    }
+    void removeById(long id);
 }
